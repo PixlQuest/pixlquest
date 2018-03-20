e4a8b7cd4c330b1ad86452218a7b2de4427178a0
Eliot Rodriguez <mr.ear84@gmail.com>
2018-03-20 15:31:21 -0400

    category word pulled from datamuse this feature is functional and ready to be integrated to the project

--- category.js
+++ category.js
@@ -1,10 +1,35 @@
-console.log("category.js is online");
+//console.log("category.js is online");
+
+/*
+	Using the Datamuse API (https://www.datamuse.com/api/) to access nouns that are associated 
+	with different colors to come up with the category word.
+*/
+
+var hold;
+var WordObject;
+var colors = ["Red", "Orange", "Yellow", "Green", "Cyan", "Blue", "Indigo", "Violet", "Purple", "Magenta", "Pink", "Brown", "White", "Gray", "Black"];
 
 
 
+//Helps to pick a random color from the colors array
+var randomColor = function(){
+	var rColor = Math.floor(Math.random() * 15);
+	//console.log(colors[rColor]);
+	return colors[rColor];
+}
+
+//pick random word from the returned object
+var wordCategory = function(result){
+	var listTotal = result.length;
+	//console.log(result.length);
+
+	var rWord = Math.floor(Math.random() * listTotal);
+	return result[rWord].word;
+}
 
 
 
+//USE ONLY WITH LOCAL HOST FOR TESTING NOT THE ACTUAL GITPAGE SERVER
 $.ajaxPrefilter(function(options) {
     if (options.crossDomain && $.support.cors) {
         options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
@@ -12,23 +37,22 @@ $.ajaxPrefilter(function(options) {
 });
 
 
-
-
+//Make a call to the Datamuse API
 $.ajax({
-	url: "https://wordsapiv1.p.mashape.com/words/?random=true",
+	url: "https://api.datamuse.com/words?rel_jja=" + randomColor(),
 	type: "GET",
-	headers: {
-		"X-Mashape-Key": "kFWOTAiw6YmshGt5R6EBnRzw6nmwp1DoErOjsnD3v6fuX1UDzz",
-		"Accept": "application/json"
-	},
-	success: function(result){
-		console.log(result);
-        console.log(result.word);
 
-        $(".catWord").text(result.word);
-	},
-	error: function(result){
-		console.log(result);
-	}
+success: function(result){
+    $(".catWord").text( wordCategory(result) );
+
+	//debugging
+	//console.log(result);
+	//hold = result;
+},
+error: function(result){
+	//console.log("it dont work" + result);
+}
+
+
 });
 
