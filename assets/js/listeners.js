d8bdd4b7aa892b97c2d97062ab5e527ac1f70b2e
Eliot <mr.ear84@gmail.com>
2018-06-09 15:42:26 -0400

    updating work done event listener on google auth now working

--- assets/js/listeners.js
+++ assets/js/listeners.js
@@ -4,9 +4,11 @@ console.log("listeners.js is online");
 define(["jquery", "main"], function ($, fireauth, main) {
 
 
+$(".loginGoogle").click(function(){
+	console.log("Google login clicked!!");
+	auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
 
-
-
+});
 
 
 
